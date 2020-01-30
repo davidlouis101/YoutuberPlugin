01f3d6d8b46d1866cc954cd1cedb80b8a99b2472
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-01-30 13:57:43 +0100

    Zest

--- src/YoutuberPlugin/Main.php
+++ src/YoutuberPlugin/Main.php
@@ -6,7 +6,7 @@ use pocketmine\plugin\pluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\commandsender
 
-class Main extends PluginBase {
+class Main extends PluginBase {}
 
 public function onEnable() : void {
  $this->getLogger->info("DC An");
