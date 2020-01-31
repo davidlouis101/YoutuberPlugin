cd1a525b1be9f784d7cbd206f0e0195de63cc270
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-01-31 21:01:06 +0100

    Update Main.php

--- src/YoutuberPlugin/Main.php
+++ src/YoutuberPlugin/Main.php
@@ -11,13 +11,13 @@ class Main extends PluginBase {
     public function onEnable(){
         $this->getLogger()->info("DC An");
     }
-    
+
     public function onCommand (CommandSender $sender, Command $command, string $label, array $args) :bool {
         switch($command->getName()){
-            case "DC":
-                $sender->sendMessage("§4Unser Discord Server --> §2https://discord.gg/rrf3gqh");
+            case "dc":
+                $sender->sendMessage("§4Unser Discord Server --> §2https://discord.gg/rrf3gqh%22);
                 $sender->sendMessage("§1Nicht Vergessen §2YT : Crow Balde");
-                $sender->sendMessage("§6Von Von§2 Crow§4 Balde:");
+                $sender->sendMessage("§6Von §2 Crow§4 Balde");
                 return true;
         }
     }
