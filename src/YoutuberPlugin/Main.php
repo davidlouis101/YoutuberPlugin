66bb1a496e07ea365d1d7d4c2bb0e884bc0b5667
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-01-31 17:32:13 +0100

    Update Main.php

--- src/YoutuberPlugin/Main.php
+++ src/YoutuberPlugin/Main.php
@@ -1,24 +1,24 @@
-<?php
+extends<?php
 
-namespace Youtuber;
+namespace YoutuberPlugin;
 
 use pocketmine\plugin\pluginBase;
 use pocketmine\command\Command;
-use pocketmine\command\commandsender;
+use pocketmine\command\CommandSender;
 
-class Main extends PluginBase {}
+class Main extends PluginBase {
 
-public function onEnable() : void {
- $this->getLogger->info("DC An");
-}
-public function onCommand (CommandSender $sender,Command $command, string $label, array $args) : bool {
- swicht($command->getName()){
-   case "YT":
-$sender->sendMassage("§4Unser Discord Server --> §2https://discord.gg/rrf3gqh");
-$sender->sendMassage("§1Nicht Vergessen §2YT : Crow Balde");
-$sender->sendMassage("VON Crow Balde");
-    Return true;
-     }
+    public function onEnable(){
+        $this->getLogger->info("DC An");
     }
-   }
+    
+    public function onCommand (CommandSender $sender, Command $command, string $label, array $args) :bool {
+        switch($command->getName()){
+            case "YT":
+                $sender->sendMessage("§4Unser Discord Server --> §2https://discord.gg/rrf3gqh");
+                $sender->sendMessage("§1Nicht Vergessen §2YT : Crow Balde");
+                $sender->sendMessage("§6Von Von§2 Crow§4 Balde:");
+                return true;
+        }
+    }
 }
